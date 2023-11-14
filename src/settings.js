98ca02a11def57141c7cb4c27f7cd6980ebee158
hjw <2232285004@qq.com>
2023-11-14 12:25:10 +0800

    初始版本

---
+++ src/settings.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+  // 网站的标题
+  title: 'Qf 人力资源后台管理系统',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  // 是否固定头部
+  fixedHeader: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  // 是否展示侧边栏的LOGO
+  sidebarLogo: true
+}
