9ceabd666599a34fdee096892a8ca27aa4844eb1
hjw <2232285004@qq.com>
2023-11-16 14:18:23 +0800

    登陆表单校验

--- src/icons/index.js
+++ src/icons/index.js
@@ -4,6 +4,10 @@ import SvgIcon from '@/components/SvgIcon'// svg component
 // register globally
 Vue.component('svg-icon', SvgIcon)
 
+// 下面三行代码的任务是 把 同级目录的 svg目录下的.svg图片引入到项目中来
 const req = require.context('./svg', false, /\.svg$/)
 const requireAll = requireContext => requireContext.keys().map(requireContext)
 requireAll(req)
+
+// 相当于把svg下的所有的svg图片打包到了项目中
+// 如果想用svg图片 就在svg目录下去寻找就可以了
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,237 +1,141 @@
 <template>
   <div class="login-container">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
+    <div class="logo"/>
+    <div class="form">
+      <h1>登录</h1>
+      <el-card shadow="never" class="login-card">
+        <!--登录表单-->
+        <el-form ref="form" :model="loginForm" :rules="loginRules">
+          <el-form-item prop="phone">
+            <el-input v-model="loginForm.phone" placeholder="请输入手机号"></el-input>
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input v-model="loginForm.password" show-password placeholder="请输入密码"></el-input>
+          </el-form-item>
+          <el-form-item prop="isAgree">
+            <el-checkbox v-model="loginForm.isAgree">
+              用户平台使用协议
+            </el-checkbox>
+          </el-form-item>
+          <el-form-item>
+            <el-button @click="login" style="width: 350px" type="primary">登陆</el-button>
+          </el-form-item>
+        </el-form>
 
-      <div class="title-container">
-        <h3 class="title">Login Form</h3>
-      </div>
-
-      <el-form-item prop="username">
-        <span class="svg-container">
-          <svg-icon icon-class="user" />
-        </span>
-        <el-input
-          ref="username"
-          v-model="loginForm.username"
-          placeholder="Username"
-          name="username"
-          type="text"
-          tabindex="1"
-          auto-complete="on"
-        />
-      </el-form-item>
-
-      <el-form-item prop="password">
-        <span class="svg-container">
-          <svg-icon icon-class="password" />
-        </span>
-        <el-input
-          :key="passwordType"
-          ref="password"
-          v-model="loginForm.password"
-          :type="passwordType"
-          placeholder="Password"
-          name="password"
-          tabindex="2"
-          auto-complete="on"
-          @keyup.enter.native="handleLogin"
-        />
-        <span class="show-pwd" @click="showPwd">
-          <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
-        </span>
-      </el-form-item>
-
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
-
-      <div class="tips">
-        <span style="margin-right:20px;">username: admin</span>
-        <span> password: any</span>
-      </div>
-
-    </el-form>
+      </el-card>
+    </div>
   </div>
 </template>
-
 <script>
-import { validUsername } from '@/utils/validate'
-
 export default {
   name: 'Login',
   data() {
-    const validateUsername = (rule, value, callback) => {
-      if (!validUsername(value)) {
-        callback(new Error('Please enter the correct user name'))
-      } else {
-        callback()
-      }
-    }
-    const validatePassword = (rule, value, callback) => {
-      if (value.length < 6) {
-        callback(new Error('The password can not be less than 6 digits'))
-      } else {
-        callback()
-      }
-    }
     return {
       loginForm: {
-        username: 'admin',
-        password: '111111'
+        phone: '',
+        password: '',
+        isAgree: ''
       },
       loginRules: {
-        username: [{ required: true, trigger: 'blur', validator: validateUsername }],
-        password: [{ required: true, trigger: 'blur', validator: validatePassword }]
-      },
-      loading: false,
-      passwordType: 'password',
-      redirect: undefined
-    }
-  },
-  watch: {
-    $route: {
-      handler: function(route) {
-        this.redirect = route.query && route.query.redirect
-      },
-      immediate: true
+        phone: [
+          {
+            required: true, message: '必填'
+          },
+          {
+            pattern: /^1[3-9]\d{9}$/, message: "手机号格式不正确"
+          }
+        ],
+        password: [
+          {
+            required: true, message: '必填'
+          },
+          {
+            min: 6, max: 16, message: "密码长度在6到16位之间"
+          }
+        ],
+        isAgree: [
+          {
+            validator: (rule, value, callback) => {
+              value ? callback() : callback(new Error("您必须勾选使用协议"))
+            }
+          }
+        ]
+      }
     }
   },
   methods: {
-    showPwd() {
-      if (this.passwordType === 'password') {
-        this.passwordType = ''
-      } else {
-        this.passwordType = 'password'
-      }
-      this.$nextTick(() => {
-        this.$refs.password.focus()
-      })
-    },
-    handleLogin() {
-      this.$refs.loginForm.validate(valid => {
-        if (valid) {
-          this.loading = true
-          this.$store.dispatch('user/login', this.loginForm).then(() => {
-            this.$router.push({ path: this.redirect || '/' })
-            this.loading = false
-          }).catch(() => {
-            this.loading = false
-          })
-        } else {
-          console.log('error submit!!')
-          return false
+    login() {
+      this.$refs.form.validate((isOK) => {
+        if (isOK) {
+          alert('校验通过')
         }
       })
     }
   }
 }
 </script>
-
 <style lang="scss">
-/* 修复input 背景不协调 和光标变色 */
-/* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
-
-$bg:#283443;
-$light_gray:#fff;
-$cursor: #fff;
-
-@supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
-  .login-container .el-input input {
-    color: $cursor;
-  }
-}
-
-/* reset element-ui css */
 .login-container {
-  .el-input {
-    display: inline-block;
-    height: 47px;
-    width: 85%;
+  display: flex;
+  align-items: stretch;
+  height: 100vh;
 
-    input {
-      background: transparent;
-      border: 0px;
-      -webkit-appearance: none;
-      border-radius: 0px;
-      padding: 12px 5px 12px 15px;
-      color: $light_gray;
-      height: 47px;
-      caret-color: $cursor;
+  .logo {
+    flex: 3;
+    background: rgba(38, 72, 176) url(../../assets/common/login_back.png) no-repeat center / cover;
+    border-top-right-radius: 60px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    justify-content: center;
+    padding: 0 100px;
 
-      &:-webkit-autofill {
-        box-shadow: 0 0 0px 1000px $bg inset !important;
-        -webkit-text-fill-color: $cursor !important;
-      }
+    .icon {
+      background: url(../../assets/common/logo.png) no-repeat 70px center /
+        contain;
+      width: 300px;
+      height: 50px;
+      margin-bottom: 50px;
     }
-  }
 
-  .el-form-item {
-    border: 1px solid rgba(255, 255, 255, 0.1);
-    background: rgba(0, 0, 0, 0.1);
-    border-radius: 5px;
-    color: #454545;
-  }
-}
-</style>
-
-<style lang="scss" scoped>
-$bg:#2d3a4b;
-$dark_gray:#889aa4;
-$light_gray:#eee;
-
-.login-container {
-  min-height: 100%;
-  width: 100%;
-  background-color: $bg;
-  overflow: hidden;
-
-  .login-form {
-    position: relative;
-    width: 520px;
-    max-width: 100%;
-    padding: 160px 35px 0;
-    margin: 0 auto;
-    overflow: hidden;
-  }
-
-  .tips {
-    font-size: 14px;
-    color: #fff;
-    margin-bottom: 10px;
-
-    span {
-      &:first-of-type {
-        margin-right: 16px;
-      }
-    }
-  }
-
-  .svg-container {
-    padding: 6px 5px 6px 15px;
-    color: $dark_gray;
-    vertical-align: middle;
-    width: 30px;
-    display: inline-block;
-  }
-
-  .title-container {
-    position: relative;
-
-    .title {
-      font-size: 26px;
-      color: $light_gray;
-      margin: 0px auto 40px auto;
+    p {
+      color: #fff;
+      font-size: 18px;
+      margin-top: 20px;
+      width: 300px;
       text-align: center;
-      font-weight: bold;
     }
   }
 
-  .show-pwd {
-    position: absolute;
-    right: 10px;
-    top: 7px;
-    font-size: 16px;
-    color: $dark_gray;
-    cursor: pointer;
-    user-select: none;
+  .form {
+    flex: 2;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding-left: 176px;
+
+    .el-card {
+      border: none;
+      padding: 0;
+    }
+
+    h1 {
+      padding-left: 20px;
+      font-size: 24px;
+    }
+
+    .el-input {
+      width: 350px;
+      height: 44px;
+
+      .el-input__inner {
+        background: #f4f5fb;
+      }
+    }
+
+    .el-checkbox {
+      color: #606266;
+    }
   }
 }
 </style>
